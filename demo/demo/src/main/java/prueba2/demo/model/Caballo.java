72a593d043332764d53010e55cd715094dc547e4
Reynaldo_Cabello <rey.cabello@duocuc.cl>
2026-05-11 16:33:00 -0400

    Guardo cambios locales antes del rebase

--- demo/demo/src/main/java/prueba2/demo/model/Caballo.java
+++ demo/demo/src/main/java/prueba2/demo/model/Caballo.java
@@ -10,7 +10,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
